6935cf8bc52cc501444d94ecac48d375396c92dc
sam <ssamiksha561@gmail.com>
2026-03-12 02:22:37 +0530

    Update solution for sample-var-std

--- sample-var-std/sample-var-std.py
+++ sample-var-std/sample-var-std.py
@@ -3,6 +3,8 @@ import numpy as np
 def sample_var_std(x):
     x=np.array(x)
     n=len(x)
+    if n < 2:
+        raise ValueError("Sample size must be at least 2 (n >= 2)")
     mean=sum(x)/n
     sqdiff=(x-mean)**2
     var=np.sum(sqdiff)/(n-1)
